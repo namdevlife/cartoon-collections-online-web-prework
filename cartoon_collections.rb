a597c2beb97953ba068a93ee4cab901f13e2419f
NAM NGUYEN <namdevlife@gmail.com>
2019-05-27 03:22:05 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+names = ["Doc", "Dopey", "Bashful", "Grumpy"]
+def roll_call_dwarves(names)# code an argument here
+  # Your code 
+    names.each_with_index puts names
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls = ["earth", "wind", "fire". "water", "heart"]
+      planeteer_calls.map { |string| string.proper } + {!}
+  
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(short)# code an argument here
   # Your code here
+  
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese_types)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  find_the_cheese(cheese_types)
 end
